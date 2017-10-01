0d71871cc938175a2c9d523e6c94409bb741c3ea
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2017-10-01 11:19:50 +0900

    add "ada88.cpp" and "ada88.h"

---
+++ BandGraphone_20171001/ada88.h
@@ -0,0 +1,1 @@
+#pragma oncevoid write_i2cDevice( unsigned char i2c, unsigned char* data, int cnt );void ada88_init( void );void ada88_write( int letter );void ada88_writeNumber( int num );	//	num 1999 .. -1999
